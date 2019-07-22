30429fe722c3960a7f9f0b415b3799af651c3760
yasmin.dias <wirecard@@019>
2019-07-22 12:24:57 -0300

    Fixes small errors

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -40,7 +40,7 @@ class AccountController < ApplicationController
   end
 
   def balance
-    render json: {account_id: @account.id, balance: @account.get_balance}, status: :unprocessable_entity
+    render json: {account_id: @account.id, balance: @account.get_balance}, status: :ok
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post 'api/auth', to: 'authentication#authenticate'
   get 'api/balance/:id', to: 'account#balance'
+  post 'api/transfer', to: 'transaction#create'
 
   scope :api do
     resources :user, :account, :transaction
